2ed738e6b29535e547a017cdf28068ccd8e6c337
Jonathan May <jonathan.may@hubbub.net>
2020-03-05 18:02:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function() {
+function() {
   let inputArea = document.getElementById("text-area");
   let text = `The app highlights lengthy, complex sentences and common errors; if you see a yellow sentence, shorten or split it. If you see a red highlight, your sentence is so dense and complicated that your readers will get lost trying to follow its meandering, splitting logic - try editing this sentence to remove the red.
 You can utilize a shorter word in place of a purple one. Mouse over them for hints.
@@ -765,4 +765,4 @@ Paste in something you're working on and edit away. Or, click the Write button a
       "we wonder": 1
     };
   }
-})();
+}();
