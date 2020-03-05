125a2c9d889cd264c38ad384fc5b837455012734
Jonathan May <jonathan.may@hubbub.net>
2020-03-05 17:44:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -51,12 +51,23 @@ Paste in something you're working on and edit away. Or, click the Write button a
   window.format = format;
   format();
 
+  function pluralize(string, data) {
+    return data > 1 ? string + "s" : string
+  }
+  
+  function generateAdverbText() {
+    if data.adverbs > Math.round(data.paragraphs / 3) {
+      return `You have used ${data.adverbs} ` + pluralize("adverb", data.adverbs) + `. 
+        We this this is a lot, in text of this length. Try to use ${Math.round(
+        data.paragraphs / 3
+      )} or fewer for text of this length.`;
+    } else {
+      return `You have used ${data.adverbs} ` + pluralize("adverb", data.adverbs) + `. 
+        We think that's about right for text of this length.`
+    }    
+  
   function counters() {
-    document.querySelector("#adverb").innerHTML = `You have used ${
-      data.adverbs
-    } adverb${data.adverbs > 1 ? "s" : ""}. Try to use ${Math.round(
-      data.paragraphs / 3
-    )} or fewer`;
+    document.querySelector("#adverb").innerHTML = generateAdverbText()
     document.querySelector(
       "#passive"
     ).innerHTML = `You have used passive voice ${data.passiveVoice} time${
