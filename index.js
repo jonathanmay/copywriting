edee48f352c10f182dfa09977596a8bc0bb42afd
Jonathan May <jonathan.may@hubbub.net>
2020-04-11 16:40:43 +0100

    Added Suggestion to highlight this is not a spellchecker.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 Getting digital copy right is hard. But, it shouldn't be as hard as it is. 
 
-Although this copywriting app is not yet a grammar or spelling checker, it highlights lengthy, complex sentences; if you see a yellow sentence, shorten or split it. If you see a red highlight, your sentence is so dense and complicated that your readers will get lost trying to follow its meandering, splitting logic - try editing this sentence to remove the red.
+This copywriting app is not yet a grammar or spelling checker. But it does a few other things pretty well. It highlights lengthy, complex sentences; if you see a yellow sentence, shorten or split it. If you see a red highlight, your sentence is so dense and complicated that your readers will get lost trying to follow its meandering, splitting logic - try editing this sentence to remove the red.
 
 Generally, you can utilize a shorter word in place of a purple one. Mouse over purple words for hints.
 
